288978f27477a29bf1564fb8474de0dbacc68f06
Harshitha Sai T S <111671338+harshitha2303@users.noreply.github.com>
2024-04-16 21:13:30 +0530

    Add files via upload
    Registration module

---
+++ UserRegistrationDto.java
@@ -0,0 +1,51 @@
+package net.javaguides.bankmanagementsystem.dto;
+
+public class UserRegistrationDto {
+
+    public UserRegistrationDto (){
+
+    }
+
+
+    private String firstName;
+    private String LastName;
+    private String email;
+    private String password;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return LastName;
+    }
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public UserRegistrationDto(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.LastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+
+
+
+    
+
+}
