bc4463aa9ff55ba32932c0b8119fc60d99a6ba74
H.V.SIRI <113015893+siri-03@users.noreply.github.com>
2024-04-16 20:51:35 +0530

    Update TransactionController.java

--- bank-management-system/bank-management-system/src/main/java/net/javaguides/bankmanagementsystem/controller/TransactionController.java
+++ bank-management-system/bank-management-system/src/main/java/net/javaguides/bankmanagementsystem/controller/TransactionController.java
@@ -1,49 +1,51 @@
 package net.javaguides.bankmanagementsystem.controller;
 
-import java.util.*;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import net.javaguides.bankmanagementsystem.entity.Transaction;
 import net.javaguides.bankmanagementsystem.service.TransactionService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
-@RequestMapping("/transactions")
 public class TransactionController {
+    
     @Autowired
     private TransactionService transactionService;
-
-    @GetMapping({"", "/"})
+    @GetMapping("/transactions")
     public String listTransactions(Model model) {
-        List<Transaction> transactions = transactionService.getAllTransactions();
-        model.addAttribute("transactions", transactions);
+        model.addAttribute("listTransactions", transactionService.getAllTransactions());
         return "transactions";
     }
 
-    @PostMapping("/deposit")
-    public ResponseEntity<String> deposit(DepositRequest request) {
-        transactionService.deposit(request.getAcFrom(), request.getAcTo(), request.getAmount());
-        return ResponseEntity.ok("Deposit successful");
+    @GetMapping("/deposit")
+    public String deposit(Model model) {
+        Transaction transaction = new Transaction();
+        model.addAttribute("transaction", transaction);
+        return "deposit";
+    }
+    
+    @PostMapping("/saveDeposit")
+    public String saveDeposit(@ModelAttribute("transaction") Transaction transaction) {
+        transactionService.saveTransaction(transaction);
+        return "redirect:/transactions";
     }
 
-    @PostMapping("/withdraw")
-    public ResponseEntity<String> withdraw(WithdrawRequest request) {
-        transactionService.withdraw(request.getAccountID(), request.getAmount());
-        return ResponseEntity.ok("Withdrawal successful");
+    @GetMapping("/withdraw")
+    public String withdraw(Model model) {
+        Transaction transaction = new Transaction();
+        model.addAttribute("transaction", transaction);
+        return "withdraw";
     }
 
-    @PostMapping("/transfer")
-    public ResponseEntity<String> transfer(TransferRequest request) {
-        transactionService.transfer(request.getFromAccountID(), request.getToAccountID(), request.getAmount());
-        return ResponseEntity.ok("Transfer successful");
+    @PostMapping("/saveWithdrawal")
+    public String saveWithdrawal(@ModelAttribute("withdraw") Transaction transaction) {
+        transactionService.saveTransaction(transaction);
+        return "redirect:/transactions";
     }
 }
