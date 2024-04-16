1092d383da0c0d31b38ee0c89d4eba34d0684d01
H.V.SIRI <113015893+siri-03@users.noreply.github.com>
2024-04-16 20:54:29 +0530

    Update TransactionService.java

--- bank-management-system/bank-management-system/src/main/java/net/javaguides/bankmanagementsystem/service/TransactionService.java
+++ bank-management-system/bank-management-system/src/main/java/net/javaguides/bankmanagementsystem/service/TransactionService.java
@@ -1,13 +1,10 @@
 package net.javaguides.bankmanagementsystem.service;
 
 import java.util.*;
-import java.math.BigDecimal;
 
 import net.javaguides.bankmanagementsystem.entity.Transaction;
 
 public interface TransactionService {
     List<Transaction> getAllTransactions();
-    void deposit(Long accountFrom, Long accountTo, BigDecimal amount);
-    void withdraw(Long accountID, BigDecimal amount);
-    void transfer(Long fromAccountID, Long toAccountID, BigDecimal amount);
+    void saveTransaction(Transaction transaction);
 }
